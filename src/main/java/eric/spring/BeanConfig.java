429239824b2637c62b31f7876fae0a8969d4121f
EricHo0129 <yung.ho@104.com.tw>
2018-12-19 15:28:16 +0800

    加入設定檔準備測試

--- src/main/java/eric/spring/BeanConfig.java
+++ src/main/java/eric/spring/BeanConfig.java
@@ -1,5 +1,59 @@
 package eric.spring;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/**
+ * 宣告所有Bean的地方，所有的controller需用import方式引入
+ * @author yung.ho
+ *
+ */
+@Configuration
+@Import({UserController.class})
 public class BeanConfig {
 
+	/**
+	 * 直接宣告產生,避免使用到預設的
+	 * @return
+	 */
+	@Bean
+	public HandlerMapping handlerMapping() {
+		return new RequestMappingHandlerMapping();
+	}
+	
+	/**
+	 * 直接宣告產生,避免使用到預設的
+	 * @return
+	 */
+	@Bean
+	public HandlerAdapter handlerAdapter() {
+		return new RequestMappingHandlerAdapter();
+	}
+	
+	/**
+	 * 非必要,若不想處理例外,可由serverless的容器來處理
+	 * @return
+	 */
+	@Bean
+	public HandlerExceptionResolver handlerExceptionResolver() {
+		return new HandlerExceptionResolver() {
+			
+			@Override
+			public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+					Exception ex) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
 }
---
+++ src/main/java/eric/spring/UserController.java
@@ -0,0 +1,23 @@
+package eric.spring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@RestController
+@EnableWebMvc
+public class UserController {
+
+	@GetMapping("/user/${pid}")
+	public Map<String, Object> getUser(@PathParam("pid") Long pid) throws Exception {
+		Map<String, Object> result = new HashMap();
+		result.put("pid", pid);
+		result.put("name", "編號"+pid+"會員");
+		return result;
+	}
+}
