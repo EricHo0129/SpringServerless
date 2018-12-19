82a730f689bdf1ae89877df12dbe1bdfea9d139e
EricHo0129 <yung.ho@104.com.tw>
2018-12-19 17:57:13 +0800

    移動設定位置

--- src/main/java/eric/spring/BeanConfig.java
+++ src/main/java/eric/spring/BeanConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
  *
  */
 @Configuration
+@EnableWebMvc
 @Import({UserController.class})
 public class BeanConfig {
 
--- src/main/java/eric/spring/UserController.java
+++ src/main/java/eric/spring/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RestController
-@EnableWebMvc
 public class UserController {
 
 	@GetMapping("/user/{pid}")
